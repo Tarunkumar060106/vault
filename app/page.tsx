a577116984adae84fa35adbc9afbe3b29940721e
S Tarunkumar <ts0147@srmist.edu.in>
2025-05-30 20:22:01 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="h1">Vault - The only storage you will need.</h1>
+    </div>
+  );
+}
