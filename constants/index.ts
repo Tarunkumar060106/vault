63f73bdf938e50d281b55cbf82f024db460370ca
S Tarunkumar <ts0147@srmist.edu.in>
2025-11-12 20:10:45 +0530

    Update MAX_FILE_SIZE to 1GB
    Increased maximum file size limit from 50MB to 1GB.

--- constants/index.ts
+++ constants/index.ts
@@ -83,4 +83,4 @@ export const sortTypes = [
 
 export const avatarPlaceholderUrl = "/assets/images/avatar.png";
 
-export const MAX_FILE_SIZE = 50 * 1024 * 1024; // 50MB
+export const MAX_FILE_SIZE = 1024 * 1024 * 1024; // 50MB
